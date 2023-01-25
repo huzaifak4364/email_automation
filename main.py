7673486a56b9aba145111aef5615af4a079c7702
Huzaifa Khan <huzaifakhan@Huzaifas-MacBook-Air.local>
2023-01-24 20:00:22 -0500

    everything

---
+++ main.py
@@ -0,0 +1,38 @@
+from googleapiclient.discovery import build
+import pandas as pd
+import time
+from send_email import send_email
+from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
+import json
+
+
+spreadsheet_id = '1422gMeYwrhGoMXasvQ8_FDoxiTAblM5yivfJyYO3w7M'
+
+range_name = 'Sheet1!A1:Z1000'
+
+creds = Credentials.from_service_account_file(
+        'email-374509-4f72425b93d2.json', scopes=["https://www.googleapis.com/auth/spreadsheets"])
+
+service = build('sheets', 'v4', credentials=creds)
+
+result = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id, range=range_name).execute()
+
+df = pd.DataFrame(result.get('values', []))
+
+initial_rows = df.shape[0]
+
+while True:
+
+    result = service.spreadsheets().values().get(
+            spreadsheetId=spreadsheet_id, range=range_name).execute()
+    df = pd.DataFrame(result.get('values', []))
+
+    current_rows = df.shape[0]
+    if current_rows > initial_rows:
+
+        send_email(df.iloc[-1])  #
+        initial_rows = current_rows
+
+    time.sleep(10)
---
+++ send_email.py
@@ -0,0 +1,101 @@
+import os
+import smtplib
+from email.message import EmailMessage
+from email.utils import formataddr
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+PORT = 587
+EMAIL_SERVER = "smtp.gmail.com"
+
+current_dir = Path(__file__).resolver(
+).parent if "_file_" in locals() else Path.cwd()
+envars = current_dir / "email.env"
+load_dotenv(envars)
+
+sender_email = os.getenv("EMAIL")
+password_email = os.getenv("PASSWORD")
+app_password = os.getenv("APP_PASSWORD")
+
+
+def send_email(row):
+    subject = "Your order is on its way"
+    name = row[0]
+    last_name = row[1]
+    reciever_email = row[2]
+    number = row[4]
+    street_address = row[5]
+    city = row[6]
+    province = row[7]
+    postal_code = row[8]
+    payment_method = row[9]
+    date = row[10]
+    items = row[13]
+
+    items_list = items.split("/n")
+    new_items = ""
+    for item in items_list:
+        new_items += "\n" + item
+
+
+
+    with open('email_template.html', 'r') as file:
+        email_template = file.read()
+    email_template = email_template.format(name=name, last_name=last_name, city=city,
+                                           province=province, postal_code=postal_code, street_address=street_address, number=number, reciever_email=reciever_email)
+
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg["From"] = formataddr(("Khana", f"{sender_email}"))
+    msg["To"] = row[0]
+
+    msg.set_content(
+        f"""\
+            First Name: {name},
+            City: {city}
+            Province: {province}
+            Postal Code: {postal_code}
+            Street Address: {street_address}
+            Number: {number}
+            Last Name: {last_name}
+            """
+
+    )
+
+    msg.add_alternative(
+        f"""\<html>
+<head>
+  <title>Order Confirmation</title>
+</head>
+<body>
+  <h1>Your order has been placed</h1>
+  <h2> Below our the details of your order</h2>
+  <p>Date:{date}</p>
+  <p>First Name:{name}</p>
+  <p>Last Name:{last_name}</p>
+  <p>Email Address:{reciever_email}</p>
+  <p>Phone Number:{number}</p>
+  <p>Street Address:{street_address}</p>
+  <p>City:{city}</p>
+  <p>Province:{province}</p>
+  <p>Postal Code:{postal_code}</p>
+  <p>Payment Method:{payment_method}</p>
+  <p>Order Details:{new_items}</p>
+  <p>If there are any concerns please reply to this email with your problem</p>
+</body>
+</html>
+""",
+subtype="html"
+    )
+        
+
+    with smtplib.SMTP(EMAIL_SERVER, PORT) as server:
+        server.starttls()
+        server.login(sender_email, app_password)
+        server.sendmail(sender_email, reciever_email, msg.as_string())
+
+
+if __name__ == "__main__":
+    send_email(row=["Arsh", "Kamran",
+               "coolarshkamran@gmail.com", "Waterloo", "647", "horizon", "", "Ontario", "L4B"])
